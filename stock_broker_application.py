30a4067bf69ae92f80ddf1bf92c126c187f7933d
doit14.jung <doit14.jung@samsung.com>
2024-06-17 14:47:06 +0900

    feature: add sell_nice_timing function & test code

--- stock_broker_application.py
+++ stock_broker_application.py
@@ -1,42 +1,33 @@
-from stock_driver import StockDriver
 import time
+
+from stock_driver import StockDriver
 
 
 class StockBrokerApplication:
     def __init__(self):
         self.__stock_broker = None
         self.__sleep_ms_between_tries = 100
-        
-    def select_stock_broker(self,
-                            stock_broker: StockDriver):
+
+    def select_stock_broker(self, stock_broker: StockDriver):
         self.__stock_broker = stock_broker
 
     def get_broker(self):
         return self.__stock_broker
 
-    def login(self,
-              _id: str,
-              pw: str) -> None:
+    def login(self, _id: str, pw: str) -> None:
         self.__stock_broker.login(_id, pw)
 
-    def purchase(self,
-                 stock_code: int,
-                 price: int,
-                 amount: int) -> None:
+    def purchase(self, stock_code: int, price: int, amount: int) -> None:
         self.__stock_broker.purchase(stock_code, price, amount)
 
-    def sell(self,
-             stock_code: str,
-             price: int,
-             amount: int) -> bool:
+    def sell(self, stock_code: str, price: int, amount: int) -> bool:
         try:
             self.__stock_broker.sell(stock_code, price, amount)
         except:
             return False
         return True
 
-    def get_price(self,
-                  stock_code: int) -> int:
+    def get_price(self, stock_code: int) -> int:
         return self.__stock_broker.get_price(stock_code)
 
     def buy_nice_timing(self, stock_code: int, money: int):
@@ -46,12 +37,18 @@ class StockBrokerApplication:
         time.sleep(self.__sleep_ms_between_tries / 1000)
         if cur_price > self.__stock_broker.get_price(stock_code):
             return 0
-  
+
         self.__stock_broker.buy(stock_code, cur_price, money // cur_price)
 
         return money // cur_price
 
-        
+    def sell_nice_timing(self, stock_code, amount):
+        prev_price = self.__stock_broker.get_price(stock_code)
+        time.sleep(self.__sleep_ms_between_tries / 1000)
+        cur_price = self.__stock_broker.get_price(stock_code)
 
-    def sell_nice_timing(self):
-        pass
+        if prev_price > cur_price:
+            self.__stock_broker.sell(stock_code, cur_price, amount)
+            return cur_price * amount
+
+        return 0
