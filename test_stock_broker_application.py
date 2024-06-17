d317a3e3770af0163d514325f513eedbb9090513
seungwoo <lirtha@naver.com>
2024-06-17 14:11:18 +0900

    sell 기능 개발 구현

--- test_stock_broker_application.py
+++ test_stock_broker_application.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
+from stock_broker_application import StockBrokerApplication
+from stock_driver import StockDriver
+
+
 class TestStockBrokerApp(TestCase):
     def test_select_stock_broker(self):
         pass
@@ -12,7 +16,13 @@ class TestStockBrokerApp(TestCase):
         pass
 
     def test_sell(self):
-        pass
+        driver: StockDriver = Mock()
+        app = StockBrokerApplication()
+        app.select_stock_broker(driver)
+        stock_code = '12341234'
+        price = 10000
+        amount = 100
+        self.assertTrue(app.sell(stock_code, price, amount))
 
     def test_get_price(self):
         pass
