a8436878262395caf383f853af22c83fee98a216
doit14.jung <doit14.jung@samsung.com>
2024-06-17 13:58:14 +0900

    feature: add login test and setUp

--- test_stock_broker_application.py
+++ test_stock_broker_application.py
@@ -1,12 +1,29 @@
+import io
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+
+from stock_broker_application import StockBrokerApplication
+
 
 class TestStockBrokerApp(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.__broker = StockBrokerApplication()
+
     def test_select_stock_broker(self):
         pass
 
-    def test_login(self):
-        pass
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_login(self, mock_stdout):
+        def print_login(id, pw):
+            print(f"{id} login success")
+
+        driver: StockBrokerApplication = Mock()
+        driver.login.side_effect = print_login
+
+        self.__broker.select_stock_broker(driver)
+        self.__broker.login("no name", "no password")
+        self.assertEqual(mock_stdout.getvalue(), "no name login success\n")
 
     def test_buy(self):
         pass
@@ -19,5 +36,6 @@ class TestStockBrokerApp(TestCase):
 
     def test_buy_nice_timing(self):
         pass
+
     def test_sell_nice_timing(self):
         pass
