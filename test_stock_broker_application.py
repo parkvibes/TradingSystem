361a8e94e3381d2a611d1adb80a44a7e8eeea36b
yezin.kim <yezin.kim@samsung.com>
2024-06-17 13:37:27 +0900

    feat: test 코드 추가

---
+++ test_stock_broker_application.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+class TestStockBrokerApp(TestCase):
+    def test_select_stock_broker(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_buy(self):
+        pass
+
+    def test_sell(self):
+        pass
+
+    def test_get_price(self):
+        pass
+
+    def test_buy_nice_timing(self):
+        pass
+    def test_sell_nice_timing(self):
+        pass
