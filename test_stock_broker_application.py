ac7bead8752c0872b18731f614fb5351ecedd032
yezin.kim <yezin.kim@samsung.com>
2024-06-17 13:51:29 +0900

    feat: test_select_stock_broker 코드 추가

--- test_stock_broker_application.py
+++ test_stock_broker_application.py
@@ -1,9 +1,16 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
+import stock_driver
+from stock_broker_application import StockBrokerApplication
+
+
 class TestStockBrokerApp(TestCase):
     def test_select_stock_broker(self):
-        pass
+        mk = Mock(spec=stock_driver)
+        app = StockBrokerApplication()
+        app.select_stock_broker(mk)
+        self.assertIs(app.get_broker(), mk)
 
     def test_login(self):
         pass
@@ -19,5 +26,6 @@ class TestStockBrokerApp(TestCase):
 
     def test_buy_nice_timing(self):
         pass
+
     def test_sell_nice_timing(self):
         pass
